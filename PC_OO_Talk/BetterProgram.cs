8751406e140a8711188f9c06ec4cf1ee29f37db8
John Townsend <john.townsend@trojans.dsu.edu>
2019-03-18 10:39:11 -0500

    Updated BetterProgram, to make it better

--- PC_OO_Talk/BetterProgram.cs
+++ PC_OO_Talk/BetterProgram.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace PC_OO_Talk
 {
@@ -9,10 +8,15 @@ namespace PC_OO_Talk
         static void Main(string[] args)
         {
             List<ProgrammingClubMember> programmingClubMembers = new List<ProgrammingClubMember>();
-            programmingClubMembers.Add(new ProgrammingClubMember(1,"John","Townsend",new DateTime(1997,4,28),"Computer Science/Math", "Chartreuse"));
-            programmingClubMembers.Add(new ProgrammingClubMember(2,"Collin","Rumpca",new DateTime(2018,3,18),"Game Design", "Gray"));
-            programmingClubMembers.Add(new ProgrammingClubMember(3,"Jarod","Keene",new DateTime(2018,3,19),"Cyber Ops", "White"));
-            programmingClubMembers.Add(new ProgrammingClubMember(4,"Brendan","Hansen",new DateTime(2018,3,20),"Literature", "Blue"));
+            var john = new ProgrammingClubMember(1, "John", "Townsend", new DateTime(1997, 4, 28), "Computer Science/Math", "Chartreuse");
+            var collin = new ProgrammingClubMember(2, "Collin", "Rumpca", new DateTime(2018, 3, 18), "Game Design", "Gray");
+            var jarod = new ProgrammingClubMember(3, "Jarod", "Keene", new DateTime(2018, 3, 19), "Cyber Ops", "White");
+            var brendan = new ProgrammingClubMember(4, "Brendan", "Hansen", new DateTime(2018, 3, 20), "Literature", "Blue");
+
+            programmingClubMembers.Add(john);
+            programmingClubMembers.Add(collin);
+            programmingClubMembers.Add(jarod);
+            programmingClubMembers.Add(brendan);
 
             //Let's print off each person and and all their attributes
 
@@ -23,7 +27,10 @@ namespace PC_OO_Talk
             
             //What else can we do to make this better?
             //How can we make our ProgrammingClubMembers more unique? Maybe some inheritance? 
-            //Should we make a 
+            //Should we make another class? Should we make our class more specific or general? 
+
+
+            //Let's maybe use a DB
            
         }
     }
